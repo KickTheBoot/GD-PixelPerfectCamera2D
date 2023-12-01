868ddce2fd6db06380e2bf0d1f39751270084b0f
Joona Myllyniemi <anoojjonttu@gmail.com>
2023-12-01 21:38:46 +0200

    Move the beginning of sizeChanged function's code inside the condition

--- PixelPerfectCamera.gd
+++ PixelPerfectCamera.gd
@@ -1,15 +1,11 @@
 extends Camera2D
 
 @export var enablePixelPerfect : bool = true
-# Defines the scale mode
-enum scaleModeEnum {VERTICAL, HORIZONTAL, SHORTEST}
-
 ## The resolution used as reference
 @export var targetResolution : Vector2i = Vector2i(256, 240)
-## Which axis should be used for scaling?
-@export var ScaleMode : scaleModeEnum = scaleModeEnum.SHORTEST
+## Often called integer scaling elsewhere.  
 ## When enabled, the adjusted resolution will snap to a multiple of the target's resolution,
-## making pixel size uniform unless one of the viewport resolution axis are less than its corresponding target.
+## making pixel size uniform unless the viewport becomes smaller than target.
 @export var MultipleOfTarget :  bool
 var viewport : Viewport
 	
@@ -20,18 +16,14 @@ func _enter_tree():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func sizeChanged():
-	var viewportresolution = viewport.get_visible_rect().size
-	var adjustedResolution = viewportresolution
-	if(MultipleOfTarget && viewportresolution.x >= targetResolution.x && viewportresolution.y >= targetResolution.y):
-			adjustedResolution -= viewportresolution.posmodv(targetResolution)
 	if(enablePixelPerfect):
+		var viewportresolution = viewport.get_visible_rect().size
+		var adjustedResolution = viewportresolution
+		if(MultipleOfTarget && viewportresolution.x >= targetResolution.x && viewportresolution.y >= targetResolution.y):
+				adjustedResolution -= viewportresolution.posmodv(targetResolution)
 		var scaleMultiplier : float = 1
-		match ScaleMode:
-			scaleModeEnum.VERTICAL:		scaleMultiplier = float(adjustedResolution.y) / float(targetResolution.y)
-			scaleModeEnum.HORIZONTAL:	scaleMultiplier = float(adjustedResolution.x) / float(targetResolution.x)
-			scaleModeEnum.SHORTEST:
-				if(float(viewportresolution.x) / float(viewportresolution.y) >= float(targetResolution.x) / float(targetResolution.y)):
-					scaleMultiplier = float(adjustedResolution.y) / float(targetResolution.y)
-				else:
-					scaleMultiplier = float(adjustedResolution.x) / float(targetResolution.x)
+		if(float(viewportresolution.x) / float(viewportresolution.y) >= float(targetResolution.x) / float(targetResolution.y)):
+			scaleMultiplier = float(adjustedResolution.y) / float(targetResolution.y)
+		else:
+			scaleMultiplier = float(adjustedResolution.x) / float(targetResolution.x)
 		zoom = Vector2.ONE * scaleMultiplier
