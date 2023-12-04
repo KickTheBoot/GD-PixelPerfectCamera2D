f90605125cff296bcd4c62e5b9daeecbaad65d15
Joona Myllyniemi <anoojjonttu@gmail.com>
2023-12-04 21:08:42 +0200

    add pixelPerfectZoom for scenarios where that could be needed, remove default comments

--- PixelPerfectCamera.gd
+++ PixelPerfectCamera.gd
@@ -3,27 +3,35 @@ extends Camera2D
 @export var enablePixelPerfect : bool = true
 ## The resolution used as reference
 @export var targetResolution : Vector2i = Vector2i(256, 240)
+@export var pixelPerfectZoom : Vector2 = Vector2(1,1) :
+	set (value):
+		pixelPerfectZoom = value
+		zoom = _scaleMultiplier * pixelPerfectZoom
+	get:
+		return pixelPerfectZoom
 ## Often called integer scaling elsewhere.  
 ## When enabled, the adjusted resolution will snap to a multiple of the target's resolution,
 ## making pixel size uniform unless the viewport becomes smaller than target.
 @export var MultipleOfTarget :  bool
 var viewport : Viewport
+ 
+var _scaleMultiplier : float = 1;
 	
 func _enter_tree():
 	viewport = get_viewport()
 	viewport.size_changed.connect(sizeChanged)
 	sizeChanged()
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func sizeChanged():
 	if(enablePixelPerfect):
 		var viewportresolution = viewport.get_visible_rect().size
 		var adjustedResolution = viewportresolution
 		if(MultipleOfTarget && viewportresolution.x >= targetResolution.x && viewportresolution.y >= targetResolution.y):
 				adjustedResolution -= viewportresolution.posmodv(targetResolution)
-		var scaleMultiplier : float = 1
+		_scaleMultiplier = 1
 		if(float(viewportresolution.x) / float(viewportresolution.y) >= float(targetResolution.x) / float(targetResolution.y)):
-			scaleMultiplier = float(adjustedResolution.y) / float(targetResolution.y)
+			_scaleMultiplier = float(adjustedResolution.y) / float(targetResolution.y)
 		else:
-			scaleMultiplier = float(adjustedResolution.x) / float(targetResolution.x)
-		zoom = Vector2.ONE * scaleMultiplier
+			_scaleMultiplier = float(adjustedResolution.x) / float(targetResolution.x)
+		zoom = Vector2.ONE * _scaleMultiplier * pixelPerfectZoom
